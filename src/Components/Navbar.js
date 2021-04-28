197294e4f55e8d5eca85292dfd52ac237fdf4ed6
fy-199 <fatihyuzbasioglu2@gmail.com>
2021-04-29 00:17:11 +0300

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+const bootstrap = require("bootstrap");
+
+function Navbar() {
+  return (
+    <div>
+      <p>Navbar gelecek</p>
+    </div>
+  );
+}
+
+export default Navbar;
